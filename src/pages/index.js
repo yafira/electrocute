bdaf02d608fa996da01de80301ca6bb8257fe86d
yafira <yafira.martinez@lc.cuny.edu>
2023-04-22 02:37:44 -0400

    added title to head

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,14 @@ import Header from 'src/components/Header'
 import Footer from 'src/components/Footer'
 import Container from '@/components/Container'
 import styles from 'src/styles/Home.module.css'
+import Head from 'next/head'
 
 export default function Home({ posts }) {
 	return (
 		<div className={styles.container}>
+			<Head>
+				<title>electrocute</title>
+			</Head>
 			<Header />
 			<Container />
 			<Footer />
