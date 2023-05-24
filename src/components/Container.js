1b0f3093a94ef5cd3ed0c8f27e27002d66c6e70a
yafira <yafira.martinez@lc.cuny.edu>
2023-05-23 23:51:01 -0400

    updated text for project card 2

--- src/components/Container.js
+++ src/components/Container.js
@@ -23,7 +23,7 @@ export default function Container() {
 				<Link href='/' className={styles.projectCard2}>
 					<Image className={styles.logo} src={Logo2} alt='project 2' />
 					<h3>*****</h3>
-					<p>work in progress ・₊✧</p>
+					<p>coming soon ・₊✧</p>
 				</Link>
 			</div>
 		</div>
