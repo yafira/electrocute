9c6e042d64754f3a8bc17e32a4ce0bb16afa49ae
yafira <yafira.martinez@lc.cuny.edu>
2023-05-31 21:12:56 -0400

    updated style of pg + new project card

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,7 @@
-import Logo from '../assets/cute-tech.png'
-import Logo2 from '../assets/bag.png'
-import Logo3 from '../assets/iclay.png'
+import Logo from '../assets/cutetech.png'
+import Logo2 from '../assets/snax.png'
+import Logo3 from '../assets/bag.png'
+import Logo4 from '../assets/iclay.png'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
@@ -21,13 +22,24 @@ export default function Container() {
 						your computing .env cuter while amplifying productivity.
 					</p>
 				</Link>
-				<Link href='/' className={styles.projectCard2}>
+
+				<Link href='https://snax.blog' className={styles.projectCard2}>
 					<Image className={styles.logo} src={Logo2} alt='project 2' />
+					<h3>snax</h3>
+					<p>
+						A blog for sharing my own reviews/ratings on vegan and plant-based
+						snacks and bevs. Also a peanut-free zone.
+					</p>
+				</Link>
+
+				<Link href='/' className={styles.projectCard3}>
+					<Image className={styles.logo} src={Logo3} alt='project 3' />
 					<h3>*****</h3>
 					<p>work in progress ・₊✧</p>
 				</Link>
-				<Link href='/' className={styles.projectCard3}>
-					<Image className={styles.logo} src={Logo3} alt='project 3' />
+
+				<Link href='/' className={styles.projectCard4}>
+					<Image className={styles.logo} src={Logo4} alt='project 4' />
 					<h3>*****</h3>
 					<p>coming soon ・₊✧</p>
 				</Link>
