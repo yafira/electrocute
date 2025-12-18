24cc7044125ee08d18124adbdf815b0e81b8a659
Yafira <ym3317@nyu.edu>
2025-12-18 15:32:05 -0500

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
 	return (
 		<div className={styles.Footer}>
 			<h3>
-				© 2023 made with{' '}
+				© 2025 made with{' '}
 				<FontAwesomeIcon icon={faHeart} style={{ color: '#B2A4D4' }} /> by
 				<Link href='https://bento.me/electrocute'> yafira </Link>
 			</h3>
