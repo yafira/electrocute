5669eaf9056f10f10d7e062a0f87a95344bd4258
Yafira <ym3317@nyu.edu>
2026-01-11 14:44:28 -0500

    added badge from internetphonebbook

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,28 @@
-import styles from '../styles/Footer.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import Link from 'next/link';
+import styles from "../styles/Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 export default function Footer() {
-	return (
-		<div className={styles.Footer}>
-			<h3>
-				© 2023 made with{' '}
-				<FontAwesomeIcon icon={faHeart} style={{ color: '#B2A4D4' }} /> by
-				<Link href='https://bento.me/electrocute'> yafira </Link>
-			</h3>
-		</div>
-	);
+  return (
+    <div className={styles.Footer}>
+      <h3>
+        © 2023 made with{" "}
+        <FontAwesomeIcon icon={faHeart} style={{ color: "#B2A4D4" }} /> by
+        <Link href="https://bento.me/electrocute"> yafira </Link>
+      </h3>
+      <div className={styles.badge}>
+        <a
+          href="https://internetphonebook.net"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://internetphonebook.net/images/badges/this-website-is-in-the-internet-phone-book-paper.png"
+            alt="this website is in the internet phone book"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
