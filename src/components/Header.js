4529b5bf1414033ebea6d195c11e5124e4d8f718
Yafira <ym3317@nyu.edu>
2026-05-11 17:29:54 -0400

    ui: updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
-import styles from '../styles/Header.module.css'
-import Link from 'next/link'
+import styles from "../styles/Header.module.css";
+import Link from "next/link";
 
 export default function Header() {
-	return (
-		<div className={styles.Header}>
-			<Link href='/'>
-				<h1>electrocute.io</h1>
-			</Link>
-		</div>
-	)
+  return (
+    <div className={styles.Header}>
+      <Link href="/">
+        <h1 className={styles.logo}>
+          <span>electrocute</span>
+          <img src="/flower.png" alt="" className={styles.flower} />
+          <span>io</span>
+        </h1>
+      </Link>
+    </div>
+  );
 }
