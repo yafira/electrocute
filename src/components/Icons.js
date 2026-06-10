26c5e0e6266cbf11f319d27771eef162124446ae
Yafira <ym3317@nyu.edu>
2026-06-10 01:10:15 -0400

    + substack link

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,6 +2,7 @@ import styles from "../styles/Icons.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import pen from "../assets/pen.png";
+import substack from "../assets/substack.png";
 import github from "../assets/github.png";
 import ig from "../assets/instagram.png";
 import fig from "../assets/figma.png";
@@ -12,71 +13,74 @@ import online from "../assets/globe.png";
 export default function Icons() {
   return (
     <div className={styles.Icons}>
-      <Link href="http://blog.electrocute.io/" title="blog">
-        <Image src={pen} alt="pen" width={25} height={25} aria-hidden="true" />
+      <Link href="http://blog.electrocute.io/" title="blog" aria-label="blog">
+        <Image src={pen} alt="" width={25} height={25} aria-hidden="true" />
       </Link>
-      <Link href="https://github.com/Yafira" title="github">
+      <Link
+        href="https://electrocute.substack.com/"
+        title="substack"
+        aria-label="substack"
+      >
         <Image
-          src={github}
-          alt="github"
+          src={substack}
+          alt=""
           width={25}
           height={25}
           aria-hidden="true"
         />
       </Link>
-      <Link href="https://www.instagram.com/electrocutelab/" title="instagram">
-        <Image
-          src={ig}
-          alt="instagram"
-          width={25}
-          height={25}
-          aria-hidden="true"
-        />
+      <Link href="https://github.com/Yafira" title="github" aria-label="github">
+        <Image src={github} alt="" width={25} height={25} aria-hidden="true" />
       </Link>
-      <Link href="https://yafira.xyz/" title="electrocute-online">
-        <Image
-          src={online}
-          alt="omg.lol"
-          width={25}
-          height={25}
-          aria-hidden="true"
-        />
+      <Link
+        href="https://www.instagram.com/electrocutelab/"
+        title="instagram"
+        aria-label="instagram"
+      >
+        <Image src={ig} alt="" width={25} height={25} aria-hidden="true" />
       </Link>
-      <Link href="https://www.figma.com/@electrocute" title="figma">
-        <Image
-          src={fig}
-          alt="figma"
-          width={25}
-          height={25}
-          aria-hidden="true"
-        />
+      <Link
+        href="https://yafira.xyz/"
+        title="electrocute-online"
+        aria-label="personal website"
+      >
+        <Image src={online} alt="" width={25} height={25} aria-hidden="true" />
       </Link>
-      <Link href="https://www.are.na/yafira/" title="arena">
+      <Link
+        href="https://www.figma.com/@electrocute"
+        title="figma"
+        aria-label="figma"
+      >
+        <Image src={fig} alt="" width={25} height={25} aria-hidden="true" />
+      </Link>
+      <Link href="https://www.are.na/yafira/" title="arena" aria-label="are.na">
         <Image
           src="https://goligorsky.neocities.org/logo-arena.svg"
-          alt="are.na logo"
+          alt=""
           width={30}
           height={30}
           aria-hidden="true"
         />
       </Link>
-      <Link href="https://www.pinterest.com/electr0cute" title="pinterest">
+      <Link
+        href="https://www.pinterest.com/electr0cute"
+        title="pinterest"
+        aria-label="pinterest"
+      >
         <Image
           src={pinterest}
-          alt="pinterest"
+          alt=""
           width={25}
           height={25}
           aria-hidden="true"
         />
       </Link>
-      <Link href="mailto:electrocutelab@gmail.com" title="email">
-        <Image
-          src={email}
-          alt="envelope"
-          width={30}
-          height={30}
-          aria-hidden="true"
-        />
+      <Link
+        href="mailto:electrocutelab@gmail.com"
+        title="email"
+        aria-label="email"
+      >
+        <Image src={email} alt="" width={30} height={30} aria-hidden="true" />
       </Link>
     </div>
   );
