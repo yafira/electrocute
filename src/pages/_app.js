9927b3c8f043a0a9b84ff511b1682f4618b6c7b4
Yafira <ym3317@nyu.edu>
2026-06-11 14:05:59 -0400

    feat: added og-image and social meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,41 @@
-import '@/styles/globals.css'
-import { motion } from 'framer-motion'
+import "@/styles/globals.css";
+import Head from "next/head";
+import { motion } from "framer-motion";
 
 export default function App({ Component, pageProps, router }) {
-	return (
-		<motion.div
-			key={router.route}
-			initial='pageInitial'
-			animate='pageAnimate'
-			variants={{
-				pageInitial: {
-					opacity: 0,
-				},
-				pageAnimate: {
-					opacity: 1,
-				},
-			}}
-		>
-			<Component {...pageProps} />
-		</motion.div>
-	)
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="electrocute.io" />
+        <meta
+          property="og:image"
+          content="https://electrocute.io/og-image.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:image"
+          content="https://electrocute.io/og-image.png"
+        />
+      </Head>
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </>
+  );
 }
