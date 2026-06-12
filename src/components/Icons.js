9fa324c5bef1eee7d0ddc49e1198f63f7de6894a
Yafira <ym3317@nyu.edu>
2026-06-12 00:07:24 -0400

    improved mobile responsiveness and rebalanced icon layout

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,6 +1,7 @@
 import styles from "../styles/Icons.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 import pen from "../assets/pen.png";
 import substack from "../assets/substack.png";
 import github from "../assets/github.png";
@@ -11,61 +12,54 @@ import email from "../assets/email.png";
 import online from "../assets/globe.png";
 
 export default function Icons({ size = 25 }) {
-  const large = size * 1.2;
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const mq = window.matchMedia("(max-width: 600px)");
+    const update = () => setMobile(mq.matches);
+    update();
+    mq.addEventListener("change", update);
+    return () => mq.removeEventListener("change", update);
+  }, []);
+
+  const s = mobile ? Math.round(size * 0.7) : size;
+  const large = Math.round(s * 1.2);
 
   return (
     <div className={styles.Icons}>
       <Link href="http://blog.electrocute.io/" title="blog" aria-label="blog">
-        <Image src={pen} alt="" width={size} height={size} aria-hidden="true" />
+        <Image src={pen} alt="" width={s} height={s} aria-hidden="true" />
       </Link>
       <Link
         href="https://electrocute.substack.com/"
         title="substack"
         aria-label="substack"
       >
-        <Image
-          src={substack}
-          alt=""
-          width={size}
-          height={size}
-          aria-hidden="true"
-        />
+        <Image src={substack} alt="" width={s} height={s} aria-hidden="true" />
       </Link>
       <Link href="https://github.com/Yafira" title="github" aria-label="github">
-        <Image
-          src={github}
-          alt=""
-          width={size}
-          height={size}
-          aria-hidden="true"
-        />
+        <Image src={github} alt="" width={s} height={s} aria-hidden="true" />
       </Link>
       <Link
         href="https://www.instagram.com/electrocutelab/"
         title="instagram"
         aria-label="instagram"
       >
-        <Image src={ig} alt="" width={size} height={size} aria-hidden="true" />
+        <Image src={ig} alt="" width={s} height={s} aria-hidden="true" />
       </Link>
       <Link
         href="https://yafira.xyz/"
         title="electrocute-online"
         aria-label="personal website"
       >
-        <Image
-          src={online}
-          alt=""
-          width={size}
-          height={size}
-          aria-hidden="true"
-        />
+        <Image src={online} alt="" width={s} height={s} aria-hidden="true" />
       </Link>
       <Link
         href="https://www.figma.com/@electrocute"
         title="figma"
         aria-label="figma"
       >
-        <Image src={fig} alt="" width={size} height={size} aria-hidden="true" />
+        <Image src={fig} alt="" width={s} height={s} aria-hidden="true" />
       </Link>
       <Link href="https://www.are.na/yafira/" title="arena" aria-label="are.na">
         <Image
@@ -81,13 +75,7 @@ export default function Icons({ size = 25 }) {
         title="pinterest"
         aria-label="pinterest"
       >
-        <Image
-          src={pinterest}
-          alt=""
-          width={size}
-          height={size}
-          aria-hidden="true"
-        />
+        <Image src={pinterest} alt="" width={s} height={s} aria-hidden="true" />
       </Link>
       <Link
         href="mailto:electrocutelab@protonmail.com"
@@ -97,8 +85,8 @@ export default function Icons({ size = 25 }) {
         <Image
           src={email}
           alt=""
-          width={size * 1.2}
-          height={size * 1.2}
+          width={large}
+          height={large}
           aria-hidden="true"
         />
       </Link>
