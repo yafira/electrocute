c514ee83ecdf548c5e751130eb0db9a76bc95cf1
Yafira <ym3317@nyu.edu>
2026-07-04 16:15:41 -0400

    fixed fontawesome issue in footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 import Head from "next/head";
 import { motion } from "framer-motion";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps, router }) {
   return (
