3a0c88a6b2f580c990d4b38ea9eac2e35c716c4b
Yafira <ym3317@nyu.edu>
2026-07-08 00:11:46 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
 import styles from "../styles/Footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className={styles.Footer}>
+    <footer className={styles.Footer}>
       <h3>
-        © 2025 crafted with{" "}
-        <FontAwesomeIcon icon={faHeart} style={{ color: "#B2A4D4" }} /> by
-        <Link href="https://yafira.xyz"> yafira </Link>
+        © 2023–2026 crafted with{" "}
+        <FontAwesomeIcon icon={faHeart} style={{ color: "#B2A4D4" }} /> by{" "}
+        <a href="https://yafira.xyz" target="_blank" rel="noopener noreferrer">
+          yafira
+        </a>
       </h3>
-      </div>
+    </footer>
   );
 }
